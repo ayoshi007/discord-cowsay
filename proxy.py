71afbf1600284f58958d580c6f7a923249ea27bc
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-08-23 22:56:27 -0500

    Rename app.py, reformat for HTTP gateway

--- proxy.py
+++ proxy.py
@@ -15,6 +15,7 @@ class InteractionType(IntEnum):
 
 class InteractionCallbackType(IntEnum):
     PONG = 1
+    CHANNEL_MESSAGE_WITH_SOURCE = 4
 
 
 PUBLIC_KEY = os.environ["DISCORD_PUBLIC_TOKEN"]
@@ -29,7 +30,25 @@ def _validate_request_headers(signature, body, timestamp):
         verify_key.verify(f"{timestamp}{body}".encode(),
                           bytes.fromhex(signature))
     except BadSignatureError:
-        raise Exception("[UNAUTHORIZED] Invalid request signature")
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 401,
+            "headers": {"content-type": "application/json"},
+            "body": "Invalid request signature"
+        }
+
+
+def handle_command(interaction):
+    print(interaction.get("name"))
+    print(interaction.get("type"))
+    print(interaction.get("data"))
+    return {
+        "type": InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+        "data": {
+            "content": "Hello from Lambda",
+        }
+    }
+    
 
 
 def lambda_handler(event, context):
@@ -37,11 +56,21 @@ def lambda_handler(event, context):
     timestamp = event["headers"].get(SIGNATURE_TIMESTAMP)
     body = event.get("body")
 
-    _validate_request_headers(signature, body, timestamp)
+    validation = _validate_request_headers(signature, body, timestamp)
+    if validation:
+        return validation
 
     interaction = json.loads(body)
+    interaction_type = interaction.get("type")
     response_json = {}
-    if interaction.get("type") == InteractionType.PING:
+    if interaction_type == InteractionType.PING:
         response_json["type"] = InteractionCallbackType.PONG
+    elif interaction_type == InteractionType.APPLICATION_COMMAND:
+        response_json = handle_command(interaction)
 
-    return response_json
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {"content-type": "application/json"},
+        "body": json.dumps(response_json)
+    }
