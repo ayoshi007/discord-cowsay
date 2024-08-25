e80af68345c79810429d0e388da0438ea14b69c2
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-08-25 00:04:47 -0500

    Remove tf files in terraform module base

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "function_name" {
-    description = "Name of Lambda function"
-    value = aws_lambda_function.discord_cowsay.function_name
-}
-
-output "base_url" {
-    description = "Base URL of API Gateway stage"
-    value = aws_apigatewayv2_stage.lambda.invoke_url
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "DISCORD_PUBLIC_TOKEN" {
-    description = "Public token to Discord"
-    sensitive = true
-}
-
-variable "LAYER_ZIP_PATH" {
-    description = "Path to Lambda layer zip archive"
-    default = "venv/layer"
-}
