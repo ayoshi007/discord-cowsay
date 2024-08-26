a0df40f4153d12806a772dbf8b14d12d0dc5cfa4
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-08-26 00:06:53 -0500

    Remove IAM roles from common module, add IAM policy for publishing to SNS topic

--- terraform/common/main.tf
+++ terraform/common/main.tf
@@ -15,43 +15,17 @@ resource "aws_sns_topic" "invoke_command" {
     name = "invoke-command-topic"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
+data "aws_iam_policy_document" "proxy_publish_sns_policy_doc" {
+  statement {
+    actions = ["sns:Publish"]
+    resources = [aws_sns_topic.invoke_command.arn]
+    effect = "Allow"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_policy" "proxy_lambda_sns_publish_policy" {
+  name = "proxy-lambda-policy"
+  description = "Policy for Discord cowsay proxy lambda"
+  policy = data.aws_iam_policy_document.proxy_publish_sns_policy_doc.json
 }
-
-# resource "aws_iam_role" "sns_publish" {
-#   name = "sns_publish"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Sid    = ""
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "sns_publish_policy" {
-#   role       = aws_iam_role.sns_publish.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
 
--- terraform/common/outputs.tf
+++ terraform/common/outputs.tf
@@ -2,14 +2,14 @@ output "invoke_command_sns_arn" {
     value = aws_sns_topic.invoke_command.arn
 }
 
-output "lambda_exec_role_arn" {
-    value = aws_iam_role.lambda_exec.arn
-}
-
 output "lambda_layer_arn" {
     value = aws_lambda_layer_version.cowsay_layer.arn
 }
 
 output "invoke_command_sns_name" {
     value = aws_sns_topic.invoke_command.name
+}
+
+output "sns_publish_policy_arn" {
+    value = aws_iam_policy.proxy_lambda_sns_publish_policy.arn
 }
