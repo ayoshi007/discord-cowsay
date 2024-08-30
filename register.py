107675f32a0304306df3600986b31adaac279f24
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-08-29 21:34:11 -0500

    Edit registration script

--- register.py
+++ register.py
@@ -14,32 +14,36 @@ headers = {
     "Authorization": f"Bot {BOT_TOKEN}"
 }
 
-def delete_commands():
-    print("Attempting to delete all existing commands")
+def delete_commands() -> int:
     response = requests.get(url, headers=headers)
     command_list = response.json()
+    print(f"Attempting to delete {len(command_list)} existing commands")
     for command in command_list:
         delete_url = f"{url}/{command['id']}"
-        response = requests.delete(url)
+        response = requests.delete(delete_url, headers=headers)
         response.raise_for_status()
         print(f"{response.status_code}: Deleted command {command['name']}")
     print(f"Successfully deleted {len(command_list)} commands")
 
-def register(json_file):
-    command_list = json.load(json_file)
-    print(f"Attempting to register {len(command_list)} commands...")
-    for command in command_list:
+def register(commands):
+    print(f"Attempting to register {len(commands)} commands...")
+    for command in commands:
         response = requests.post(url, headers=headers, json=command)
         response.raise_for_status()
         print(f"{response.status_code}: Registered command {command['name']}")
-    print(f"Successfully registered {len(command_list)} commands")
+    print(f"Successfully registered {len(commands)} commands")
+    
+def run(json_file):
+    if not os.path.exists(json_file):
+        raise FileNotFoundError(f"Could not find {json_file}")
+    delete_commands()
+    with open(json_file) as f:
+        commands = json.load(f)
+    register(commands)
     
 
 if __name__ == "__main__":
-    JSON_ARG = "json"
-    args = sys.argv[1:]
-    parser = argparse.ArgumentParser()
-    parser.add_argument(JSON_ARG, required=True, help="Path to JSON file of commands to register")
-    parsed = parser.parse_args(args)
-    delete_commands()
-    register(parsed[JSON_ARG])
+    parser = argparse.ArgumentParser(prog="Discord register", description="A registration script for Discord global or guild commands")
+    parser.add_argument("json", help="Path to JSON file of commands to register")
+    parsed = parser.parse_args(sys.argv[1:])
+    run(parsed.json)
