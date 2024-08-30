96222476638cbbcb324d131ee091b8fb499109e4
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-08-29 21:19:19 -0500

    Edit blop command

--- commands/blop.py
+++ commands/blop.py
@@ -1,14 +1,22 @@
-def handler(event, context):
-    for record in event['Records']:
-        process_message(record)
-    print("done")
+import requests
 
-def process_message(record):
-    try:
-        message = record['Sns']['Message']
-        print(f"Processed message {message}")
-        # TODO; Process your record here
-        
-    except Exception as e:
-        print("An error occurred")
-        raise e
+callback_url = "https://discord.com/api/v10/interactions/{}/{}/callback"
+
+def handler(event, context):
+    records = event["Records"]
+    print(records)
+    data = json.loads(records[0]["Sns"]["Message"])
+    interaction_id = data["interaction_id"]
+    interaction_token = data["interaction_token"]
+    options = data.get("options", [])
+    option_values = [option.get("value") for option in options]
+    url = callback_url.format(interaction_id, interaction_token)
+    r = requests.post(url, json={
+        "type": 4,
+        "data": {
+            "content": f"blep {option_values}"
+        }
+    })
+    r.raise_for_status()
+    print(r.json())
+    
