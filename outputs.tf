12ed3406f86d3871272a1bb00fd76e9f179e0fa5
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-10-05 16:34:38 -0500

    Remove SNS topic from outputs

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,6 @@ output "base_url" {
     value = module.proxy.base_url
 }
 
-output "sns_topic" {
-    description = "Name of command invoking SNS topic"
-    value = module.common.invoke_command_sns_name
-}
-
 output "dynamodb_table" {
     description = "Name of DynamoDB table of quotes"
     value = module.cowquote_infra.dynamodb_table_name
